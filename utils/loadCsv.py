2b1dcc5525923eb1f3d44e453e8255731dd2619e
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2022-06-05 17:10:56 +0900

    csv読み込みまで

---
+++ utils/loadCsv.py
@@ -0,0 +1,21 @@
+import csv
+
+
+def loadCsv(csvFile='./data/kinmu.xlsx', userRowStart=35, dateLine=0, dataLineStart=3, dataLineEnd=33):
+    # 31日までの月はdateLineEnd=33
+    csv_input = []
+    with open(csvFile, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            csv_input.append(row)
+    # 勤務情報の抜き出し
+    user_input = csv_input[userRowStart:userRowStart + 3]
+    user_input = [row[dataLineStart:dataLineEnd + 1] for row in user_input]
+    # 日付の抜き出し
+    date_input = csv_input[dateLine][dataLineStart:dataLineEnd + 1]
+
+    return user_input, date_input
+
+
+if __name__ == '__main__':
+    print(loadCsv('./data/2022年6月.csv', dataLineEnd=32))
